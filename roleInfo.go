b9cf430d13f6734278ee24a2b0e0c9d908f11a60
Maverick Wolf <76170873+Maverick-Wolf@users.noreply.github.com>
2021-01-04 10:31:47 +0530

    improve command

--- roleInfo.go
+++ roleInfo.go
@@ -1,14 +1,17 @@
 {{/*A role info command.
 	Usuage: 
     
-    -roleinfo <role ID> 
+    -roleinfo <role ID/role Name/role Mention> 
     
     Recommended trigger: Regex trigger with trigger `\A(-|<@!?204255221017214977>\s*)(roleinfo|ri)(\s+|\z)`
     NOTE : For any further doubts or bugs you can contact me on discord "Maverick Wolf#1010"
 */}}
-
-{{$args := parseArgs 1 "" (carg "int" "role id of the role")}}
-{{$role := (.Guild.Role ($args.Get 0))}}
+{{$role := ""}}
+{{$input := .StrippedMsg}}
+{{if reFind `\d{17,19}` $input}}
+{{$input = reFind `\d{17,19}` $input}}
+{{end}}
+{{range .Guild.Roles}}{{if eq (str $input|lower) (str .ID) (lower .Name)}}{{$role = .}}{{end}}{{end}}
 {{$all_perms := ""}}
 {{$ex := "png" }}{{if reFind "a_" .Guild.Icon}}{{$ex = "gif" }}{{end}}{{$icon := print "https://cdn.discordapp.com/icons/" .Guild.ID "/" .Guild.Icon "." $ex "?size=512" }}
 {{if $role}}
@@ -17,9 +20,9 @@
   {{range seq 0 (len $perms_list)}}
      {{if (mod $perm 2)}}
 	{{if $all_perms}}
-	  {{$all_perms = (print $all_perms ", " (index $perms_list .))}}
+	  {{$all_perms = (print $all_perms ", `" (index $perms_list .) "`")}}
 	{{else}}
-	  {{$all_perms = (index $perms_list .)}}
+	  {{$all_perms = (print "`" (index $perms_list .) "`")}}
 	{{end}}
      {{end}}
         {{$perm = (div $perm 2)}}
@@ -30,11 +33,11 @@
                                      (sdict "name" "Position" "value" (print (sub (len .Guild.Roles) $role.Position)) "inline" true)
                                      (sdict "name" "Hoisted" "value" (print $role.Hoist) "inline" true)
   			             (sdict "name" "Managed" "value" (print $role.Managed) "inline" true)
-  			             (sdict "name" "Permissions" "value" (print "`" $all_perms "`"))) "color" $role.Color
+  			             (sdict "name" "Permissions" "value" (print $all_perms))) "color" $role.Color
 		  "author" (sdict "name" .User.Username "icon_url" (.User.AvatarURL "512"))
 		  "thumbnail" (sdict "url" $icon)
 		  "footer" (sdict "text" .Server.Name) "timestamp" currentTime)}}
 {{sendMessage nil $embed}}
 {{else}}
-`Please enter the correct role ID`
+`Please enter the correct role ID/role Name/role Metion`
 {{end}}
