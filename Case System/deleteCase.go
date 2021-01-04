a1ac537f286b39431b2847d75ba1b2cf1f6ead4a
Maverick Wolf <76170873+Maverick-Wolf@users.noreply.github.com>
2021-01-04 09:27:37 +0530

    improve code

--- Case System/deleteCase.go
+++ Case System/deleteCase.go
@@ -1,11 +1,10 @@
 {{/*This command allows you to delete a perticular case using the case number.
 
      Usuage: 
-    
+
     -delcase <case_number>
-    
+
     Recommended trigger: Regex trigger with trigger `\A(-|<@!?204255221017214977>\s*)(delcase|deletecase|clearcase|dc|clc)(\s+|\z)`
-    NOTE : For any further doubts or bugs you can contact me on discord "Maverick Wolf#1010"
 */}}
 {{$args := parseArgs 1 "correct usuage is -delcase case_number" (carg "int" "case number")}}
 {{$caseno := ($args.Get 0)}}
@@ -13,4 +12,4 @@
 {{dbDel $caseno "viewcase"}}
 {{dbDel $caseno $id}}
 {{dbDel $caseno "userid"}}
-{{sendMessage nil "Deleted the case"}}
+`Deleted the case`
