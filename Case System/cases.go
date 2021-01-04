4be684baa125ca7ce43a5445e7ff86127b34e3f8
Maverick Wolf <76170873+Maverick-Wolf@users.noreply.github.com>
2021-01-04 09:26:28 +0530

    minor changes

--- Case System/cases.go
+++ Case System/cases.go
@@ -1,15 +1,14 @@
 {{/* This command allows you to see all the cases that a user has.
 
      Usuage: 
-    
+
     -cases <user> [page] where page is optional 
-    
+
     Recommended trigger: Regex trigger with trigger `\A(-|<@!?204255221017214977>\s*)(cases|allcase)(\s+|\z)`
-    NOTE : For any further doubts or bugs you can contact me on discord "Maverick Wolf#1010"
 */}}
 {{$args := parseArgs 1 "correct usuage is -cases @user" (carg "member" "target user") (carg "int" "page number")}}
 {{$user := ($args.Get 0).User}}
-{{$id := ($args.Get 0).User.ID}}
+{{$id := $user.ID}}
 {{$display := ""}}
 {{$page := 1}} {{/* Default page to start at */}}
 {{if ($args.IsSet 1)}}
@@ -20,12 +19,11 @@
 {{end}}{{end}}
 {{$skip := mult (sub $page 1) 10}}
 {{$cases := dbTopEntries $id 10 $skip}}
-{{if not $cases}}
-{{$display = "`No Cases exists on this page`"}}
+{{range $cases}}
+		{{- $value := str .Value }} 
+		{{- $display = joinStr "" $display "\n" $value -}} 
 {{else}}
-{{- range $cases -}}
-		{{ $value := str .Value }} 
-		{{ $display = joinStr "" $display "\n" $value}} 
-{{- end -}}{{- end -}}
+{{$display = "`No cases exist on this page`"}}
+{{end}}
 {{$id := sendMessageRetID nil (cembed "author" (sdict "name" (print $user.Username " (" $user.ID ")") "icon_url" ($user.AvatarURL "512")) "title" "Cases" "description" $display "footer" (sdict "text" (print "Page " $page)) "color" 0x00ffd9)}}
 {{addMessageReactions nil $id "◀️" "▶️"}}
